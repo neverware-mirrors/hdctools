61d6a466cf73302b5eea83b56d625e6247af409c
Ruben Rodriguez Buchillon <coconutruben@chromium.org>
2018-07-24 20:48:22 +0800

    servod: measure_power: add more data retrieval mechanisms
    This CL adds the ability to save:
    - save the raw data collected
    - summary
    - the logs
    from the measure_power.py
    
    BUG=chromium:806146
    TEST=tested out savings and they work as expected
    
    Change-Id: Ief8ce53ad0419090ae8ac1bf3027ed24ec98d50b
    Signed-off-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1131035
    Reviewed-by: Todd Broch <tbroch@chromium.org>

--- servo/dut_power.py
+++ servo/dut_power.py
@@ -6,12 +6,41 @@
 from __future__ import print_function
 import argparse
 import logging
+import os
+import shutil
 import sys
+import tempfile
 
 import client
 # This module is just a wrapper around measure_power functionality
 import measure_power
 
+
+def _AddMutuallyExclusiveAction(name, parser, default=True, action='save'):
+  """Add both '--do-something' and '--no-do-something' pair to parser.
+
+  This adds a mutually exclusive switch for a boolean action into a parser.
+  Adds two flags:
+  --%{action}-%{name}
+  --no-%{action}-%{name}
+
+  Args:
+    name: object on which to perform the action
+    parser: parser to attach mutually exclusive group to
+    default: default value for boolean switch
+    action: action to perform on name
+  """
+
+  saver = parser.add_mutually_exclusive_group()
+  argname = '--%s-%s' % (action, name)
+  noargname = '--no-%s-%s' % (action, name)
+  dest = '%s_%s' % (action, name.replace('-', '_'))
+  arghelp = '%s %s' % (action, name)
+  saver.add_argument(argname, default=default, dest=dest,
+                     action='store_true', help=arghelp)
+  noarghelp = "don't %s %s" % (action, name)
+  saver.add_argument(noargname, default=default, dest=dest,
+                     action='store_false', help=noarghelp)
 
 def main(cmdline=sys.argv[1:]):
   parser = argparse.ArgumentParser(description='Measure power using servod.')
@@ -37,7 +66,21 @@ def main(cmdline=sys.argv[1:]):
                       help='print debug log')
   parser.add_argument('--no-output', default=False, action='store_true',
                       help='do not output anything into stdout')
+  parser.add_argument('-o', '--outdir', default=None,
+                      help='directory to save data into')
+  parser.add_argument('-m', '--message', default=None,
+                      help='message to append to each summary file stored')
+  _AddMutuallyExclusiveAction('raw-data', parser, default=False)
+  _AddMutuallyExclusiveAction('summary', parser)
+  # NOTE: if logging gets too verbose, turn default off
+  _AddMutuallyExclusiveAction('logs', parser)
+  parser.add_argument('--save-all', default=False,
+                      help='Equivalent to --save-summary --save-logs '
+                      '--save-raw-data. Overwrites any of those if specified.')
   args = parser.parse_args(cmdline)
+  # Save all logic
+  if args.save_all:
+    args.save_logs = args.save_raw_data = args.save_summary = True
   pm_logger = logging.getLogger('')
   pm_logger.setLevel(logging.DEBUG)
   stdout_handler = logging.StreamHandler(sys.stdout)
@@ -46,12 +89,28 @@ def main(cmdline=sys.argv[1:]):
     stdout_handler.setLevel(logging.DEBUG)
   if not args.no_output:
     pm_logger.addHandler(stdout_handler)
+  if args.save_logs:
+    tmplogfile = tempfile.NamedTemporaryFile()
+    logfilehandler = logging.StreamHandler(tmplogfile)
+    logfilehandler.setLevel(logging.DEBUG)
+    pm_logger.addHandler(logfilehandler)
   pm = measure_power.PowerMeasurement(host=args.host, port=args.port,
                                       ina_rate=args.ina_rate,
                                       vbat_rate=args.vbat_rate,
                                       fast=args.fast)
   pm.MeasureTimedPower(sample_time=args.time, wait=args.wait)
   pm.DisplaySummary()
+  if args.save_summary:
+    pm.SaveSummary(args.outdir, args.message)
+  if args.save_raw_data:
+    pm.SaveRawData(args.outdir)
+  if args.save_logs:
+    outdir = pm._outdir
+    if args.outdir and os.path.isdir(args.outdir):
+      outdir = args.outdir
+    logfile = os.path.join(outdir, 'logs.txt')
+    pm_logger.info('Storing logs at:\n%s', logfile)
+    shutil.move(tmplogfile.name, logfile)
 
 if __name__ == '__main__':
   main(sys.argv[1:])
