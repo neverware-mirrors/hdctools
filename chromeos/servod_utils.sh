933e3ee1e3579122ed8784de7cdede1b051f3f84
Nick Sanders <nsanders@chromium.org>
2018-07-26 18:42:00 -0700

    labstation: restart usb interface on servod restart
    We cache the usb topology of this device on bootup,
    and if we restart servod, we'll restart the USB interface
    at the level of servo_v4 hub to get a fresh start.
    
    BUG=chromium:830133
    TEST=ran unittest for servod_utils
    
    Change-Id: I6e30397343420c59dd04056173ecbd0a6e3553bb
    Signed-off-by: Nick Sanders <nsanders@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1152549
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Richard Barnette <jrbarnette@google.com>

--- chromeos/servod_utils.sh
+++ chromeos/servod_utils.sh
@@ -19,3 +19,66 @@ update_config() {
     echo "$key=$value" >> $config_file
   fi
 }
+
+cache_servov4_hub() {
+  local config_file=$1
+  local serial=$2
+
+  if [ -z "${serial}" ]; then
+    return
+  fi
+
+  # Find this servo if it is a servo_v4, check its serialno
+  SERVOPATH=$(grep -l "^${serial}\$" /sys/bus/usb/devices/*/serial)
+
+  if [ -n "${SERVOPATH}" ]; then
+    SERVOV4=$(dirname "${SERVOPATH}")
+
+    logger -t "$UPSTART_JOB" "Servo: ${serial} found at: ${SERVOV4}"
+
+    # The hub is one level up.
+    SERVOV4_HUB=$(echo "${SERVOV4}" | sed 's/..$//')
+
+    logger -t "$UPSTART_JOB" "Servo Hub is: ${SERVOV4_HUB}"
+
+    if [ -n "${SERVOV4_HUB}" ]; then
+      if [ -f "${SERVOV4_HUB}"/authorized ]; then
+        # If this hub actually exists, let's save the path for later.
+        update_config "${config_file}" HUB "${SERVOV4_HUB}"
+      else
+        logger -t "$UPSTART_JOB" "File not found: ${SERVOV4_HUB}/authorized"
+      fi
+    else
+      logger -t "$UPSTART_JOB" "No hub detected for ${serial}"
+    fi
+  else
+    logger -t "$UPSTART_JOB" "No servo detected for ${serial}"
+  fi
+}
+
+slam_servov4_hub() {
+  local hub=$1
+
+  if [ -n "${hub}" ]; then
+    if [ -f "${hub}"/authorized ]; then
+      logger -t "$UPSTART_JOB" "Restarting USB interface on ${hub}"
+      echo 0 > "${hub}"/authorized
+      sleep 1
+      echo 1 > "${hub}"/authorized
+      sleep 3
+    else
+      logger -t "$UPSTART_JOB" "Hub control ${hub}/authorized doesn't exist"
+    fi
+  fi
+}
+
+# For testing:
+test_servo_utils () {
+  logger() {
+    echo $@
+  }
+  cache_servov4_hub "test.config"
+  cache_servov4_hub "test.config" "Uninitialized"
+  . ./test.config
+  slam_servov4_hub $HUB
+}
