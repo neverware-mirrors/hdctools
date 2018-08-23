1b6d792a65c198d29b5711d0a0b72f86b27ba297
Nicolas Boichat <drinkcat@chromium.org>
2018-08-23 09:05:53 +0800

    whiskers: Delete obsolete whiskers overlay, fix INA map
    BUG=none
    TEST=emerge -av hdctools
    
    Change-Id: I8673697450f53cf1f84fb5c8153aa0ab9356c24b
    Reviewed-on: https://chromium-review.googlesource.com/1186004
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- servo/data/whiskers.py
+++ servo/data/whiskers.py
@@ -3,6 +3,7 @@
 # found in the LICENSE file.
 
 inas = [
+        ('ina3221', '0x40:0', 'ppvar_kbd_bl',  7.7, 0.020, 'rem', True),
         ('ina3221', '0x40:1', 'pp3300_bec',    3.3, 0.020, 'rem', True),
         ('ina3221', '0x40:2', 'pp3300_tp',     3.3, 0.020, 'rem', True),
 ]
