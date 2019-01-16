bc182ac972ddf651afdb43ff800a3eaf5b53127e
Duncan Laurie <dlaurie@chromium.org>
2019-01-15 18:36:56 -0800

    sarien: Add sarien to the list of boards to use atmega
    There is a static list of boards that use atmega for the keyboard,
    and sarien needs to be added to this list.
    
    This needs refactoring to allow this to be specified in the board XML.
    
    BUG=b:121380403
    TEST=pass firmware_DevBootUSB
    
    Change-Id: I8dfc08bc60c20514951b4e36cb0ceeabb0e31d60
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1413781
    Commit-Ready: Duncan Laurie <dlaurie@google.com>
    Tested-by: Duncan Laurie <dlaurie@google.com>
    Reviewed-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>

--- servo/servo_server.py
+++ servo/servo_server.py
@@ -218,9 +218,9 @@ class Servod(object):
     elif board == 'stout':
       return keyboard_handlers.StoutHandler(servo)
     elif board in ('buddy', 'cranky', 'guado', 'jecht', 'mccloud', 'monroe',
-                   'ninja', 'nyan_kitty', 'panther', 'rikku', 'stumpy', 'sumo',
-                   'tidus', 'tricky', 'veyron_fievel', 'veyron_mickey',
-                   'veyron_rialto', 'veyron_tiger', 'zako'):
+                   'ninja', 'nyan_kitty', 'panther', 'rikku', 'sarien',
+                   'stumpy', 'sumo', 'tidus', 'tricky', 'veyron_fievel',
+                   'veyron_mickey', 'veyron_rialto', 'veyron_tiger', 'zako'):
       if self._usbkm232 is None:
         logging.info('No device path specified for usbkm232 handler. Use '
                      'the servo atmega chip to handle.')
