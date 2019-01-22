adde376e892428c33c27aa14383b3e7a7f41f2ad
Duncan Laurie <dlaurie@google.com>
2019-01-22 10:55:48 -0800

    sarien_power: Make power_state:on work if power is already on
    To ensure a power_state:on always results in a system that is powered
    on, first do a cold reset and then press the power button.
    
    BUG=b:121380403
    TEST=dut-control power_state:on power_state:on
    
    Change-Id: I9565fec769ab52ca6a1fe6dacc77bc318d976d51
    Signed-off-by: Duncan Laurie <dlaurie@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/1427140
    Commit-Ready: ChromeOS CL Exonerator Bot <chromiumos-cl-exonerator@appspot.gserviceaccount.com>
    Reviewed-by: Simon Glass <sjg@chromium.org>

--- servo/drv/sarien_power.py
+++ servo/drv/sarien_power.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 import time
@@ -12,10 +12,13 @@ class sarienPower(power_state.PowerStateDriver):
   # 'rec_mode' signal after cold reset.
   _RECOVERY_DETECTION_DELAY = 5
 
+  # Time in seconds to wait before taking action after cold reset.
+  _COLD_RESET_DELAY = 3
+
   def _reset_cycle(self):
     """Force a power cycle using cold reset."""
     self._cold_reset()
-    time.sleep(3.0)
+    time.sleep(self._COLD_RESET_DELAY)
     self._interface.power_short_press()
 
   def _power_on_rec(self):
@@ -32,6 +35,8 @@ class sarienPower(power_state.PowerStateDriver):
 
   def _power_on(self, rec_mode):
     """Power on in normal or recovery mode."""
+    self._cold_reset()
+    time.sleep(self._COLD_RESET_DELAY)
     if rec_mode == self.REC_ON:
       self._power_on_rec()
     else:
@@ -40,4 +45,4 @@ class sarienPower(power_state.PowerStateDriver):
   def _power_off(self):
     """Force power off, even if already off."""
     self._cold_reset()
-    time.sleep(3.0)
+    time.sleep(self._COLD_RESET_DELAY)
