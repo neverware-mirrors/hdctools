a9884e5338ae48424a3b413f72b3531ae8710dd2
Mary Ruthven <mruthven@chromium.org>
2019-07-31 11:35:01 -0700

    stm32uart: temporary workaround for timeout issue
    BUG=chromium:936182
    BRANCH=none
    TEST=run servo_micro_version 1000 times on labstaton. Make sure it
    doesn't timeout.
    
    Change-Id: Icab83be0b40fa96ebdc870400909bec90414f0af
    Signed-off-by: Mary Ruthven <mruthven@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hdctools/+/1729759
    Reviewed-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>
    Commit-Queue: Ruben Rodriguez Buchillon <coconutruben@chromium.org>

--- servo/stm32uart.py
+++ servo/stm32uart.py
@@ -121,6 +121,9 @@ class Suart(uart.Uart):
         try:
           if readp.poll(.1):
             r = os.read(self._ptym, 64)
+            # TODO(crosbug.com/936182): Remove when the servo v4/micro console
+            # issues are fixed.
+            time.sleep(0.001)
             if r:
               self._susb._write_ep.write(r, self._susb.TIMEOUT_MS)
 
