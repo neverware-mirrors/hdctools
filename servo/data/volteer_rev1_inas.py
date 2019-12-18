4e3aefbf00a6833187eee2188d32d0d4df469bf2
Keith Short <keithshort@chromium.org>
2019-12-18 14:42:19 -0700

    volteer: Add INA definition
    Add INA defines for the Volteer reference board.
    
    BUG=b:146646417
    BRANCH=none
    TEST=emerge hdctools
    TEST=Start servod, verify INA measurements using dut-control
    
    Change-Id: I87200191c190921817f4be66ff93c749bcd6e6fb
    Signed-off-by: Keith Short <keithshort@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hdctools/+/1979008
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Reviewed-by: Eric Herrmann <eherrmann@chromium.org>
    Reviewed-by: Abe Levkoy <alevkoy@chromium.org>

---
+++ servo/data/volteer_rev1_inas.py
@@ -0,0 +1,21 @@
+# Copyright 2019 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+config_type='servod'
+
+inas = [
+#    drvname,   slv,      name,               nom,  sense, mux,   is_calib
+    ('ina3221', '0x40:0', 'ppvar_vccin_aux',  1.80, 0.002, 'rem', True), # R571
+    ('ina3221', '0x40:1', 'pp1100_dram',      1.10, 0.002, 'rem', True), # R592
+    ('ina3221', '0x40:2', 'pp3300_ssd_a',     3.30, 0.020, 'rem', True), # R276
+    ('ina3221', '0x41:0', 'ppvar_vnn_bypass', 1.05, 0.010, 'rem', True), # R572
+    ('ina3221', '0x41:1', 'pp1050_a_bypass',  1.05, 0.010, 'rem', True), # R573
+    ('ina3221', '0x41:2', 'pp1800_a',         1.80, 0.010, 'rem', True), # R526
+    ('ina3221', '0x42:0', 'pp3300_a',         3.30, 0.005, 'rem', True), # R532
+    ('ina3221', '0x42:1', 'pp3300_g',         3.30, 0.010, 'rem', True), # R518
+    ('ina3221', '0x42:2', 'pp5000_a',         5.00, 0.002, 'rem', True), # R527
+    ('ina3221', '0x43:0', 'pp3300_wwan_dx',   3.30, 0.010, 'rem', True), # R385
+    ('ina3221', '0x43:1', 'pp3300_wlan_a',    3.30, 0.020, 'rem', True), # R645
+    ('ina3221', '0x43:2', 'pp3300_edp_dx',    3.30, 0.020, 'rem', True), # R648
+]
