0d9c90c9ea718a1a7d5bae985bc59768e45ef1c5
Mengqi Guo <mqg@chromium.org>
2020-01-03 13:52:27 -0800

    kohaku: Add new INA mapping
    BUG=b:134092071
    TEST=dut-power
    able to see readings for each rail
    
    Change-Id: I00a9021ebee6b6e099013a150b92dd1089c1a48b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hdctools/+/1989885
    Reviewed-by: Todd Broch <tbroch@chromium.org>
    Commit-Queue: Mengqi Guo <mqg@chromium.org>
    Tested-by: Mengqi Guo <mqg@chromium.org>
    Auto-Submit: Mengqi Guo <mqg@chromium.org>

---
+++ servo/data/kohaku_rev6_loc1.py
@@ -0,0 +1,9 @@
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+config_type='sweetberry'
+
+inas = [
+    ('sweetberry', '0x40:3', 'vbat', 7.70, 0.010, 'j2', True), # R569
+]
