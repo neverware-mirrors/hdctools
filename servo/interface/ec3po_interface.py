62cabb24fde33edae868d9e6283a7a78565ea611
Ruben Rodriguez Buchillon <coconutruben@chromium.org>
2020-05-12 15:40:11 -0700

    ec3po_interface: import dummy
    On servo v2, we might fail to initialize the ec3p0 interface and fall
    back to just adding a dummy interface. For that to be a successful
    endeavor, the dummy package has to be imported.
    
    BUG=chromium:1081808
    TEST=sudo emerge hdctools -j32
         sudo servod -b kevin
         // happy
    
    Change-Id: Ie37407156dffdd84bb19e66668ddede93ad94a5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hdctools/+/2197739
    Tested-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>
    Tested-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Commit-Queue: Ruben Rodriguez Buchillon <coconutruben@chromium.org>

--- servo/interface/ec3po_interface.py
+++ servo/interface/ec3po_interface.py
@@ -19,6 +19,7 @@ import time
 import tty
 
 import common as c
+from . import dummy
 from ec3po import console
 from ec3po import interpreter
 from ec3po import threadproc_shim
@@ -246,7 +247,7 @@ class EC3PO(uart.Uart):
       # initializing this interface since no control relies on it.
       c.build_logger.debug('Skip initializing EC3PO for %s, no control '
                            'specified.', raw_uart_name)
-      return dummy.Build()
+      return dummy.Dummy.Build()
 
   @staticmethod
   def name():
