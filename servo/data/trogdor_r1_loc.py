24d097e13b9a6ea14ebe2c70220431ff8cf09ee7
Matthias Kaehlcke <mka@chromium.org>
2020-06-19 12:51:08 -0700

    servo: data: trogdor: Add medusa configuration for rev1
    Add medusa configuration for trogdor rev1
    
    BUG=b:157666809
    TEST=use sweetberry to measure power on trogdor rev1
    
    Change-Id: I2271aedd028b240c8d0662ba4d373b7ed36bd3f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hdctools/+/2276750
    Tested-by: Matthias Kaehlcke <mka@chromium.org>
    Reviewed-by: Matthias Kaehlcke <mka@chromium.org>
    Commit-Queue: Matthias Kaehlcke <mka@chromium.org>

---
+++ servo/data/trogdor_r1_loc.py
@@ -0,0 +1,25 @@
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+config_type='sweetberry'
+
+inas = [
+    ('sweetberry', '0x40:3', 'PPVAR_BAT',          8.05, 0.010, 'j2', True), # R172_33
+    ('sweetberry', '0x40:1', 'PP3300_HUB',         8.05, 0.100, 'j2', True), # R108, original size 0.01
+    ('sweetberry', '0x40:2', 'PP5000_A',           5.00, 0.010, 'j2', True), # R4689, orginal size 0.002
+    ('sweetberry', '0x40:0', 'PP3300_A_R',         3.30, 0.010, 'j2', True), # R4699, orginal size 0.002
+    ('sweetberry', '0x41:3', 'PP3300_EC_STBY',     3.30, 0.010, 'j2', True), # R297_FF
+    ('sweetberry', '0x41:1', 'PP3300_H1',          3.30, 0.010, 'j2', True), # R296_FF
+    ('sweetberry', '0x41:2', 'PP1800_VR',          3.30, 0.010, 'j2', True), # R312
+    ('sweetberry', '0x41:0', 'PP1800_EC',          3.30, 0.010, 'j2', True), # R330
+    ('sweetberry', '0x42:3', 'PP1800_SENSORS',     3.30, 0.010, 'j2', True), # R43
+    ('sweetberry', '0x42:1', 'PP3300_PD_EC',       3.30, 0.100, 'j2', True), # R297, original size 0.01
+    ('sweetberry', '0x42:2', 'PP3300_PD_A',        3.30, 0.100, 'j2', True), # R397_FF, original size 0.01
+    ('sweetberry', '0x42:0', 'PP3300_EC',          3.30, 0.100, 'j2', True), # R398_FF, original size 0.01
+    ('sweetberry', '0x43:3', 'PP1200_TCPC_C0',     3.30, 0.010, 'j2', True), # R268
+    ('sweetberry', '0x43:1', 'LCD_BL_VOUT',        3.30, 0.010, 'j2', True), # R227
+    ('sweetberry', '0x43:2', 'PP3300_TCPC_C0',     3.30, 0.100, 'j2', True), # R4708, original size 0.01
+    ('sweetberry', '0x43:0', 'PPVAR_BAT_R',        3.30, 0.100, 'j2', True), # Across battery
+    ('sweetberry', '0x44:3', 'PP1200_BRIJ',        3.30, 0.100, 'j3', True), # R93_LS, original size 0.01
+]
