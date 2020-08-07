4a7b3c5ccc1b50c0d46d4c5e50806953706005f1
Ruben Rodriguez Buchillon <coconutruben@chromium.org>
2020-08-06 19:27:29 -0700

    v4p1: inas
    This change adds the INAs for v4p1
    
    BUG=b:154436412
    TEST=sudo servod -b volteer
         dut-control ppdut5_mv
    ppdut5_mv:15123.75
    
    Change-Id: I6500b661cfc24b06be47d3cbe74f61bcfeb0d36a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/hdctools/+/2342094
    Tested-by: Ruben Rodriguez Buchillon <coconutruben@chromium.org>
    Reviewed-by: Brian Nemec <bnemec@chromium.org>
    Commit-Queue: Ruben Rodriguez Buchillon <coconutruben@chromium.org>

---
+++ servo/data/servo_v4p1_inas.py
@@ -0,0 +1,11 @@
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+inas = [
+        # TODO(nsanders): Come up with a way to support multiple voltages.
+        ('ina231', 0x40, 'ppdut5', 5.0, 0.005, 'rem', True),
+        ('ina231', 0x41, 'ppchg5', 5.0, 0.005, 'rem', True),
+        ('ina231', 0x42, 'ppservo5', 5.0, 0.005, 'rem', True),
+       ]
+# Must be servo v4p1's i2c interface array index + 1 from servo_interface.py.
+interface = 23
